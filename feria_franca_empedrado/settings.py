a478b8b7de6d2425e800b536c3129536df226d28
Bernardo Javier Garcia <119458353+BJavierG@users.noreply.github.com>
2023-07-09 13:20:31 -0300

    Update settings.py

--- feria_franca_empedrado/settings.py
+++ feria_franca_empedrado/settings.py
@@ -129,13 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/statics/'
-#STATICFILES_DIRS = [
-#    BASE_DIR / "statics"
-#]
-
-STATIC_ROOT = [
+STATICFILES_DIRS = [
     BASE_DIR / "statics"
 ]
+
+#STATIC_ROOT = [
+#    BASE_DIR / "statics"
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
